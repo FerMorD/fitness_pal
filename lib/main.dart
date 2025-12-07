d3a68b3a5a2fb7048aa5363f4b35d3f0060ed2ca
FerMorD <fernmorenod@gmail.com>
2025-12-06 18:02:31 -0600

    Had to create a new file in VS code to see the progress of my app. This is a given template which allows me to actually play around and actively see what I am doing.

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
